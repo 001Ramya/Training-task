f721c489fb14a9673d9363debf85b159fc5248cf
001Ramya <ramyasivakumar08112007@gmail.com>
2026-02-07 17:42:43 +0530

    Create Tribonacci Number.c

---
+++ Tribonacci Number.c
@@ -0,0 +1,17 @@
+int tribonacci(int n) {
+    int i,a=0,b=1,c=1,d=0;
+    if(n==1)
+     return 1;
+    else if(n==2)
+     return 1;
+    
+    for(i=3;i<=n;i++)
+    {
+        d=a+b+c;
+        a=b;
+        b=c;
+        c=d;
+    }
+    return d;
+    
+}
