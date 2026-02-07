dbec506e300426e9e7a6420996c066e77b79579d
001Ramya <ramyasivakumar08112007@gmail.com>
2026-02-07 17:35:20 +0530

    Create Triangle Type Detector.c

---
+++ Triangle Type Detector.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if((a==b&&b==c&&c==a))
+        printf("Equilateral Triangle");
+    else if((a==b)||(b==c)||(c==a))
+        printf("Isosceles Triangle");
+    else if(a+b>c&&b+c>a&&c+a>b)
+        printf("Scalene Triangle");
+    else
+        printf("Not a valid triangle");
+    return 0;
+}
