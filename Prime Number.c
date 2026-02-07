8cb2794931847fa362a319bab8291240c2f95e2f
001Ramya <ramyasivakumar08112007@gmail.com>
2026-02-07 17:38:40 +0530

    Create Prime Number.c

---
+++ Prime Number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n,i;
+    scanf("%d",&n);
+    if(n==1)
+    {
+        printf("Neither Prime Nor Composite");
+        return 0;
+    }
+        
+    for(i=2;i*i<=n;i++)
+    {
+     if(n%i==0)
+     {
+         printf("Not Prime");
+         return 0;
+     }
+        
+    }
+    printf("Prime");
+    
+    return 0;
+}
