835d23b9c15c4d7173fc1bc27257462102b45e2e
001Ramya <ramyasivakumar08112007@gmail.com>
2026-02-07 17:53:41 +0530

    Create Rectangle Patteren.c

---
+++ Rectangle Patteren.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int a,b,i,j;
+    scanf("%d%d",&a,&b);
+    for(i=0;i<a;i++)
+    {
+        for(j=0;j<b;j++)
+        {
+          printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
