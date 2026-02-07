2b5746ffeb75ac12faec9adf10459253b650337a
001Ramya <ramyasivakumar08112007@gmail.com>
2026-02-07 17:52:31 +0530

    Create Water bottles.c

---
+++ Water bottles.c
@@ -0,0 +1,13 @@
+int numWaterBottles(int numBottles, int numExchange) {
+    
+    long total=numBottles;
+    int empty=numBottles;
+
+    while(empty>=numExchange)
+    {
+       int newBottles=empty/numExchange;
+       total+=newBottles;
+       empty=newBottles+(empty%numExchange);
+    }
+    return total;
+}
